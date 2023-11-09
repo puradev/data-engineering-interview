63b82fbf36024a5e905971bc6ea2d11c97cb52fc
chris_fraleigh <christopher.fraleigh@gmail.com>
2023-11-09 09:43:20 -0800

    better name for url envar

--- app/random_user_loader.py
+++ app/random_user_loader.py
@@ -2,11 +2,12 @@ from requests import Response, Session, exceptions as re
 import json
 import os
 import logging
-endpoint_url = os.environ["RANDO_API_URL"]
+endpoint_url = os.environ["RANDOM_USER_API_URL"]
 
 
 class RandomUserLoader:
     def _get_users_json(self, url: str) -> list[dict]:
+        #make call to api and get list of random users
         new_session = Session()
         try:
             logging.info("Requesting user list from server")
@@ -19,6 +20,7 @@ class RandomUserLoader:
         return response["results"]
 
     def _parse_user_json(self, user: dict) -> dict:
+        #parse user json dict into the fields selected
         user_dict = {
             "first_name": user.get("name", "N/A").get("first", "N/A"),
             "last_name": user.get("name", "N/A").get("last", "N/A"),
@@ -39,6 +41,7 @@ class RandomUserLoader:
         # Ensure that the target directory exists, create it if not
         os.makedirs(path, exist_ok=True)
 
+        #open file and write out parsed json to new line delimited json file
         with open(f"{path}/user_information.json", "w", encoding="utf-8") as user_file:
             for user in users:
                 user_dict = self._parse_user_json(user)
